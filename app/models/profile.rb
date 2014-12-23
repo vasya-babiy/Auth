9ae2a594c40e72524c98f29e57bb4593d7d908e3
vasya-babiy <basek.93@gmail.com>
2014-12-23 17:36:55 +0200

    Add picture for user

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :age, :first_name, :last_name
+  attr_accessible :age, :first_name, :last_name, :image
+  mount_uploader :image, ImageUploader
   # validates :first_name, :last_name, :age, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141211090509) do
+ActiveRecord::Schema.define(:version => 20141218101158) do
 
   create_table "profiles", :force => true do |t|
     t.string   "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20141211090509) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
   add_index "profiles", ["user_id"], :name => "index_profiles_on_user_id"
