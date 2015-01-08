803ad037fe43daaabd54eadb5c2cf70e4e6cfe45
vasya-babiy <basek.93@gmail.com>
2015-01-08 18:16:06 +0200

    edit profile with modal window and ajax

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,11 @@ class ProfilesController < ApplicationController
 	def show
 		@user = User.find(params[:user_id])
 		@profile = @user.profile
+
+		respond_to do |format|
+      format.html {}
+      format.js {}
+    end 
 	end
 
 	def edit
@@ -16,7 +21,7 @@ class ProfilesController < ApplicationController
 		respond_to do |format|
 
 			if @profile.update_attributes(params[:profile])
-				format.html {redirect_to user_profile_path(@user)}
+				format.html {redirect_to user_profile_path(@user), notice: "Profile changed"}
 				format.js {}
 			else
 				format.html {render 'edit'}
